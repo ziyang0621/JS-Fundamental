d4176286f765b1c53317749a9a65be6b23b70342
Ziyang tan <ziyang0621@gmail.com>
2018-04-26 18:52:21 -0700

    52 functional programming 2

--- playground/app.js
+++ playground/app.js
@@ -33,3 +33,14 @@ var checkPastLimitSimplied = function(limiter) {
 
 var arr5 = mapForEach(arr1, checkPastLimitSimplied(1));
 console.log(arr5);
+
+var arr6 = _.map(arr1, function(item) {
+  return item * 3;
+});
+console.log(arr6);
+
+var arr7 = _.filter([2, 3, 4, 5, 6, 7], function(item) {
+  return item % 2 === 0;
+});
+console.log(arr7);
+
