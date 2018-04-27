2d36825600d77bb1cf2baa8232c8cc8b4239cbc8
Ziyang tan <ziyang0621@gmail.com>
2018-04-26 18:46:20 -0700

    48 function factories

--- playground/app.js
+++ playground/app.js
@@ -1,35 +1,16 @@
-function buildFunctions() {
-  var arr = [];
-  for (var i = 0; i < 3; i++) {
-    arr.push(function() {
-      console.log(i);
-    });
-  }
-
-  return arr;
+function makeGreeting(language) {
+  return function(firstname, lastname) {
+    if (language === 'en') {
+      console.log('Hello ' + firstname + ' ' + lastname);
+    }
+    if (language === 'es') {
+      console.log('Hola ' + firstname + ' ' + lastname);
+    }
+  };
 }
 
-var fs = buildFunctions();
-fs[0]();
-fs[1]();
-fs[2]();
+var greetEnglish = makeGreeting('en');
+var greetSpanish = makeGreeting('es');
 
-function buildFunctions2() {
-  var arr = [];
-  for (var i = 0; i < 3; i++) {
-    arr.push(
-      (function(j) {
-        return function() {
-          console.log(j);
-        };
-      })(i)
-    );
-  }
-
-  return arr;
-}
-
-var fs2 = buildFunctions2();
-fs2[0]();
-fs2[1]();
-fs2[2]();
+greetEnglish('John', 'Doe');
+greetSpanish('John', 'Doe');
