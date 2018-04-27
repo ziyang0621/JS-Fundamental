c2a3b56454df939c29676ac7a3d9d15f3dd37f66
Ziyang tan <ziyang0621@gmail.com>
2018-04-26 18:49:42 -0700

    50 call() apply() bind()

--- playground/app.js
+++ playground/app.js
@@ -1,24 +1,48 @@
-function sayHiLater() {
-  var greeting = 'Hi!';
+var person = {
+  firstname: 'John',
+  lastname: 'Doe',
+  getFullName: function() {
+    var fullname = this.firstname + ' ' + this.lastname;
+    return fullname;
+  }
+};
 
-  setTimeout(function() {
-    console.log(greeting);
-  }, 3000);
+var logName = function(lang1, lang2) {
+  console.log('Logged: ' + this.getFullName());
+  console.log('Arguments: ' + lang1 + ' ' + lang2);
+  console.log('------------');
+};
+
+var logPersonName = logName.bind(person);
+
+logPersonName('en');
+
+logName.call(person, 'en', 'es');
+logName.apply(person, ['en', 'es']);
+
+(function(lang1, lang2) {
+  console.log('Logged: ' + this.getFullName());
+  console.log('Arguments: ' + lang1 + ' ' + lang2);
+  console.log('------------');
+}.apply(person, ['en', 'es']));
+
+// function borrowing
+
+var person2 = {
+  firstname: 'Jane',
+  lastname: 'Doe'
+};
+
+console.log(person.getFullName.apply(person2));
+
+// function currying
+
+function multiply(a, b) {
+  return a * b;
 }
 
-sayHiLater();
+var multiplyByTwo = multiply.bind(this, 2);
+console.log(multiplyByTwo(4));
 
-function tellMeWhenDone(callback) {
-  var a = 1000;
-  var b = 2000;
-
-  callback();
-}
-
-tellMeWhenDone(function() {
-  console.log('I am done!');
-});
-
-tellMeWhenDone(function() {
-  console.log('All done!');
-});
+var multiplyByThree = multiply.bind(this, 3);
+console.log(multiplyByThree(4));
