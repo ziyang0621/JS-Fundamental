c679a000db11ddc7401c2afb25e2583322838713
Ziyang tan <ziyang0621@gmail.com>
2018-04-27 18:15:48 -0700

    55 understanding prototype

--- playground/app.js
+++ playground/app.js
@@ -1,46 +1,24 @@
-function mapForEach(arr, fn) {
-  var newArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    newArr.push(fn(arr[i]));
+var person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  getFullName: function() {
+    return this.firstname + ' ' + this.lastname;
   }
-  return newArr;
-}
-
-var arr1 = [1, 2, 3];
-console.log(arr1);
-
-var arr2 = mapForEach(arr1, function(item) {
-  return item * 2;
-});
-console.log(arr2);
-
-var arr3 = mapForEach(arr1, function(item) {
-  return item > 2;
-});
-console.log(arr3);
-
-var checkPastLimit = function(limiter, item) {
-  return item > limiter;
-};
-var arr4 = mapForEach(arr1, checkPastLimit.bind(this, 1));
-console.log(arr4);
-
-var checkPastLimitSimplied = function(limiter) {
-  return function(limiter, item) {
-    return item > limiter;
-  }.bind(this, limiter);
 };
 
-var arr5 = mapForEach(arr1, checkPastLimitSimplied(1));
-console.log(arr5);
+var john = {
+  firstname: 'John',
+  lastname: 'Doe'
+};
 
-var arr6 = _.map(arr1, function(item) {
-  return item * 3;
-});
-console.log(arr6);
+// don't do this EVER! for demo purpose only!!
+john.__proto__ = person;
+console.log(john.getFullName());
+console.log(john.firstname);
 
-var arr7 = _.filter([2, 3, 4, 5, 6, 7], function(item) {
-  return item % 2 === 0;
-});
-console.log(arr7);
+var jane = {
+  firstname: 'Jane'
+};
 
+jane.__proto__ = person;
+console.log(jane.getFullName());
