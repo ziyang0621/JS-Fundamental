69a63af44aa922da7f77b94c47f02052c6798ba9
Ziyang tan <ziyang0621@gmail.com>
2018-05-01 13:25:05 -0700

    64 ES6 classes

--- playground/app.js
+++ playground/app.js
@@ -1,26 +1,27 @@
-// polyfill
-if (!Object.create) {
-  Object.create = function(o) {
-    if (arguments.length > 1) {
-      throw new Error(
-        'Object.create implementaton' + ' only accepts the first parameter.'
-      );
-    }
-    function F() {}
-    F.prototype = o;
-    return new F();
-  };
-}
+// this is still an object, not a class
+class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
 
-var person = {
-  firstname: 'Default',
-  lastname: 'Default',
-  greet: function() {
+  greet() {
     return 'Hi ' + this.firstname;
   }
-};
+}
 
-var john = Object.create(person);
-john.firstname = 'John';
-john.lastname = 'Doe';
+var john = new Person('John', 'Doe');
 console.log(john);
+
+class InformalPerson extends Person {
+  constructor(firstname, lastname) {
+    super(firstname, lastname);
+  }
+
+  greet() {
+    return 'Yo ' + this.firstname;
+  }
+}
+
+var jane = new InformalPerson('Jane', 'Doe');
+console.log(jane);
