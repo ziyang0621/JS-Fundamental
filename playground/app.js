fec9afd42c4fd0881307cb4d7f1e9481fc7f5c52
Ziyang tan <ziyang0621@gmail.com>
2018-05-03 11:58:09 -0700

    67 strict mode

--- playground/app.js
+++ playground/app.js
@@ -1,27 +1,13 @@
-var a = 3;
-console.log(typeof a);
+function logNewPerson() {
+  'use strict';
 
-var b = 'Hello';
-console.log(typeof b);
-
-var c = {};
-console.log(typeof c);
-
-var d = [];
-console.log(typeof d);
-console.log(Object.prototype.toString.call(d));
-
-function Person(name) {
-  this.name = name;
+  var person2;
+  persom2 = {};
+  console.log(persom2);
 }
 
-var e = new Person('Jane');
-console.log(typeof e);
-console.log(e instanceof Person);
+var person;
+persom = {};
+console.log(persom);
+logNewPerson();
 
-console.log(typeof undefined);
-console.log(typeof null);
-
-var z = function() {};
-console.log(typeof z);
-console.log(z instanceof Object);
