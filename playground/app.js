f376e9ee57e0b2417bbddcac92294cd69cf63141
Ziyang tan <ziyang0621@gmail.com>
2018-05-08 20:15:23 -0700

    72 jQuery

--- playground/app.js
+++ playground/app.js
@@ -1,13 +1,4 @@
-function logNewPerson() {
-  'use strict';
-
-  var person2;
-  persom2 = {};
-  console.log(persom2);
-}
-
-var person;
-persom = {};
-console.log(persom);
-logNewPerson();
-
+var q = $('ul.people')
+  .addClass('newClass')
+  .removeClass('people');
+console.log(q);
