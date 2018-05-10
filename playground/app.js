ee46a52c984b3bddcec8aaee964252ca36fc0967
Ziyang tan <ziyang0621@gmail.com>
2018-05-10 13:36:26 -0700

    79 framework

--- playground/app.js
+++ playground/app.js
@@ -1,4 +1,17 @@
-var q = $('ul.people')
-  .addClass('newClass')
-  .removeClass('people');
-console.log(q);
+var g = G$('John', 'Doe');
+g
+  .greet()
+  .setLang('es')
+  .greet(true)
+  .log();
+
+$('#login').click(function() {
+  var loginGrtr = G$('John', 'Doe');
+
+  $('#logindiv').hide();
+
+  loginGrtr
+    .setLang($('#lang').val())
+    .HTMLGreeting('#greeting', true)
+    .log();
+});
